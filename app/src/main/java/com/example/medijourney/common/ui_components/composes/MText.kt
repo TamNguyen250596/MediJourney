a7ddb9199de5955db9f2bccbe91078d368c7eb53
TomNguyen <minhtamnguyen@motorist.vn>
2025-07-30 19:03:01 +0700

    Refactor: Remove unused import
    The unused `Color` import from `androidx.compose.ui.graphics.Color` has been removed from `MText.kt`.

--- app/src/main/java/com/example/medijourney/common/ui_components/composes/MText.kt
+++ app/src/main/java/com/example/medijourney/common/ui_components/composes/MText.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
